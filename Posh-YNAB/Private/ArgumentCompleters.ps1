6d9c18ddb04938361ae06a86294d08803bcbe2bd
Connor Griffin <connor.griffin@azdoa.gov>
2018-03-29 14:34:41 -0700

    Added AccountName ArgumentCompleter

--- Posh-YNAB/Private/ArgumentCompleters.ps1
+++ Posh-YNAB/Private/ArgumentCompleters.ps1
@@ -1,4 +1,4 @@
-# Profile Name tab completion
+# Tab completers
 $budgetName = @{
     CommandName = $paramsByFunction.Where{$_.Parameter -contains 'BudgetName'}.Function
     Parameter = 'BudgetName'
@@ -33,4 +33,91 @@ $budgetName = @{
     }
 }
 
+$budgetId = @{
+    CommandName = $paramsByFunction.Where{$_.Parameter -contains 'BudgetID'}.Function
+    Parameter = 'BudgetID'
+    ScriptBlock = {
+        param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+        # Get the token value from the pipeline or PSDefaultParamterValues
+        if ($fakeBoundParameter.Token) {
+            $token = $fakeBoundParameter.Token
+        } elseif ($PSDefaultParameterValues["${commandName}:Token"]) {
+            $token = $global:PSDefaultParameterValues["${commandName}:Token"]
+        }
+
+        # Only continue trying to complete if a token was provided
+        if ($token) {
+            # Get a list of all budgets
+            $budgets = Get-YNABBudget -Token $token -List | Sort BudgetID
+
+            # Trim quotes from the $wordToComplete
+            $wordMatch = $wordToComplete.Trim("`"`'")
+
+            # Add a CompletionResult for each budget name matching wordToComplete
+            $budgets.Where{$_.BudgetID -like "*$wordMatch*"}.ForEach{
+                New-Object System.Management.Automation.CompletionResult (
+                    "`"$($_.BudgetID)`"",
+                    $_.BudgetID,
+                    'ParameterValue',
+                    $_.BudgetID
+                )
+            }
+        }
+    }
+}
+
+$accountName = @{
+    CommandName = $paramsByFunction.Where{$_.Parameter -contains 'AccountName'}.Function
+    Parameter = 'AccountName'
+    ScriptBlock = {
+        param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+        # Get the token value from the pipeline or PSDefaultParamterValues
+        if ($fakeBoundParameter.Token) {
+            $token = $fakeBoundParameter.Token
+        } elseif ($PSDefaultParameterValues["${commandName}:Token"]) {
+            $token = $global:PSDefaultParameterValues["${commandName}:Token"]
+        }
+
+        # Get the budget ID or name from the pipeline or PSDefaultParamterValues
+        if ($fakeBoundParameter.BudgetName -or $fakeBoundParameter.BudgetID) {
+            $budgetName = $fakeBoundParameter.BudgetName
+            $budgetId = $fakeBoundParameter.BudgetID
+        }
+        elseif ($PSDefaultParameterValues["${commandName}:BudgetName"] -or $PSDefaultParameterValues["${commandName}:BudgetID"]) {
+            $budgetName = $global:PSDefaultParameterValues["${commandName}:BudgetName"]
+            $budgetId = $global:PSDefaultParameterValues["${commandName}:BudgetID"]
+        }
+
+        # Build a parameter object for splatting
+        $params = @{List = $true}
+        if ($token) {$params.Token = $token}
+        # Prioritize ID over name, only include one
+        elseif ($budgetId) {$params.BudgetID = $budgetId}
+        if ($budgetName) {$params.BudgetName = $budgetName}
+
+        # Only continue trying to complete if a token was provided
+        if ($token -and ($budgetId -or $budgetName)) {
+            # Get a list of all accounts
+            $accounts = Get-YNABAccount @params | Sort Name
+
+            # Trim quotes from the $wordToComplete
+            $wordMatch = $wordToComplete.Trim("`"`'")
+
+            # Add a CompletionResult for each budget name matching wordToComplete
+            $accounts.Where{$_.Name -like "*$wordMatch*"}.ForEach{
+                New-Object System.Management.Automation.CompletionResult (
+                    "`"$($_.Name)`"",
+                    $_.Name,
+                    'ParameterValue',
+                    $_.Name
+                )
+            }
+        }
+    }
+}
+
 Register-ArgumentCompleter @budgetName
+Register-ArgumentCompleter @budgetId
+Register-ArgumentCompleter @accountName
--- Posh-YNAB/Public/Get-YNABAccount.ps1
+++ Posh-YNAB/Public/Get-YNABAccount.ps1
@@ -13,16 +13,16 @@ function Get-YNABAccount {
     .PARAMETER logname
     The name of a file to write failed computer names to. Defaults to errors.txt.
     #>
-    [CmdletBinding(DefaultParameterSetName='List')]
+    [CmdletBinding(DefaultParameterSetName='List:BudgetID')]
     param(
         [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName,Mandatory=$true,ParameterSetName='DetailByBudgetName,AccountName')]
         [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName,Mandatory=$true,ParameterSetName='DetailByBudgetName,AccountID')]
-        [Parameter(ParameterSetName='List')]
+        [Parameter(ParameterSetName='List:BudgetName')]
         [String]$BudgetName,
 
         [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName,Mandatory=$true,ParameterSetName='DetailByBudgetID,AccountName')]
         [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName,Mandatory=$true,ParameterSetName='DetailByBudgetID,AccountID')]
-        [Parameter(ParameterSetName='List')]
+        [Parameter(ParameterSetName='List:BudgetID')]
         [String]$BudgetID,
 
         [Parameter(ValueFromPipelineByPropertyName,ParameterSetName='DetailByBudgetName,AccountName')]
@@ -36,21 +36,24 @@ function Get-YNABAccount {
         [Parameter(Mandatory=$true)]
         [String]$Token,
 
-        [Parameter(ParameterSetName='List')]
+        [Parameter(ParameterSetName='List:BudgetName')]
+        [Parameter(ParameterSetName='List:BudgetID')]
         [Switch]$List
     )
 
     begin {
         # Set the default header value for Invoke-RestMethod
         $header = Get-Header $Token`
-        Write-Verbose "ParameterSetName: $($PsCmdlet.ParameterSetName)"
+        Write-Verbose "Get-YNABAccount ParameterSetName: $($PsCmdlet.ParameterSetName)"
     }
 
     process {
         # Get the budget IDs if the budget was specified by name
         if ($BudgetName) {
+            Write-Verbose "Performing budget lookup to get BudgetID for $BudgetName"
             $budgets = Get-YNABBudget -List -Token $Token
             $BudgetID = $budgets.Where{$_.Name -like $BudgetName}.BudgetID
+            Write-Verbose "Using BudgetID: $BudgetID"
         }
 
         # Get the account ID if the account was specified by name
@@ -60,10 +63,11 @@ function Get-YNABAccount {
                 $name = $_
                 $accounts.Where{$_.Name -like $name}.AccountID
             }
+            Write-Verbose "Using AccountID: $($AccountID -join ', ')"
         }
 
         switch -Wildcard ($PsCmdlet.ParameterSetName) {
-            'List' {
+            'List*' {
                 $response = Invoke-RestMethod "$uri/budgets/$BudgetID/accounts" -Headers $header
                 if ($response) {
                     Get-ParsedAccountJson $response.data.accounts
