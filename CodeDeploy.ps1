083b58a96aea949483553f7c12abd61639e93eba
Connor Griffin <connor@connorcg.com>
2018-04-04 21:46:17 -0700

    Testing

--- CodeDeploy.ps1
+++ CodeDeploy.ps1
@@ -1,3 +1,4 @@
+[cmdletBinding()]
 param($Phase)
 
 Switch ($Phase) {
@@ -6,6 +7,6 @@ Switch ($Phase) {
     'Build' {
         $localModPath = $env:PSModulePath.Split(':')[0]
         Copy-Item -Recurse ./Posh-YNAB/ $localModPath
-        Publish-Module -Name 'Posh-YNAB' -NugetApiKey $ENV:PSGalleryAPIKey -ErrorAction Stop
+        Publish-Module -Name 'Posh-YNAB' -NugetApiKey $ENV:PSGalleryAPIKey
     }
 }
