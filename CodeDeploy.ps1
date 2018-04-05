ce0e8438830edcfeff18654a4531705ea246ca99
Connor Griffin <connor@connorcg.com>
2018-04-04 20:46:28 -0700

    Build testing

--- CodeDeploy.ps1
+++ CodeDeploy.ps1
@@ -2,9 +2,8 @@ param($Phase)
 
 Switch ($Phase) {
     'Install' {
-        Install-Module PSDeploy -Force
     }
     'Build' {
-        Invoke-PSDeploy -Tags Remote -Force
+        Write-Host $env:PSModulePath
     }
 }
