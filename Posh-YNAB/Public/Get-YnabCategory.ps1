96abce00989738486b448f9f502406d58a1f36b5
Connor Griffin <connor@connorcg.com>
2018-06-03 23:09:20 -0700

    Cleaned up parameter sets, added pipeline support,

--- Posh-YNAB/Public/Get-YnabCategory.ps1
+++ Posh-YNAB/Public/Get-YnabCategory.ps1
@@ -13,66 +13,54 @@ function Get-YnabCategory {
     .PARAMETER logname
     The name of a file to write failed computer names to. Defaults to errors.txt.
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='List')]
     param(
-        [Parameter(Mandatory=$true,ValueFromPipeline,ValueFromPipelineByPropertyName,ParameterSetName='Detail:BudgetName,CategoryName')]
-        [Parameter(Mandatory=$true,ValueFromPipeline,ValueFromPipelineByPropertyName,ParameterSetName='Detail:BudgetName,CategoryID')]
-        [Parameter(Mandatory=$true,ParameterSetName='List:BudgetName')]
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
         [String]$Budget,
 
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ParameterSetName='Detail:BudgetName,CategoryName')]
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ParameterSetName='Detail:BudgetID,CategoryName')]
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName='Detail')]
         [String[]]$Category,
 
-        [Parameter(ParameterSetName='List:BudgetName')]
-        [Parameter(ParameterSetName='List:BudgetID')]
-        [Switch]$List,
+        [Parameter(ValueFromPipelineByPropertyName,
+                   ParameterSetName='List')]
+        [Switch]$ListAll,
 
-        [Parameter(ParameterSetName='List:BudgetName')]
-        [Parameter(ParameterSetName='List:BudgetID')]
+        [Parameter(ValueFromPipelineByPropertyName,
+                   ParameterSetName='List')]
         [Switch]$IncludeHidden,
 
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
         $Token
     )
 
     begin {
-        Write-Verbose "Get-YnabCategory.ParameterSetName: $($PsCmdlet.ParameterSetName)"
-        
-        # Set the default header value for Invoke-RestMethod
         $header = Get-Header $Token
     }
 
     process {
-        # Get the budget IDs if the budget was specified by name
-        if ($BudgetName) {
-            $budgets = [Array](Get-YnabBudget -List -Token $Token)
-            $BudgetID = $budgets.Where{$_.Budget -like $BudgetName}.BudgetID
-        }
+        # Get the budget and category data
+        $budgets = [Array](Get-YnabBudget -ListAll -Token $Token)
+        $budgetId = $budgets.Where{$_.Budget -like $Budget}.BudgetID
 
-        # Get the account ID if the account was specified by name
-        if ($CategoryName) {
-            $categories = (Get-YnabCategory -List -BudgetID $BudgetID -Token $Token).Categories
-            $CategoryID = $CategoryName.ForEach{
-                $name = $_
-                $categories.Where{$_.Category -like $name}.CategoryID
-            }
-        }
-
-        switch -Wildcard ($PsCmdlet.ParameterSetName) {
-            'List*' {
-                $response = Invoke-RestMethod "$uri/budgets/$BudgetID/categories" -Headers $header
-                if ($response) {
-                    Get-ParsedCategoryJson $response.data.category_groups -List -IncludeHidden:$IncludeHidden
+        switch ($PsCmdlet.ParameterSetName) {
+            'List' {
+                $categories = Invoke-RestMethod "$uri/budgets/$budgetId/categories" -Headers $header
+                # Return the full list of categories by group
+                if ($categories) {
+                    Get-ParsedCategoryJson $categories.data.category_groups -List -IncludeHidden:$IncludeHidden
                 }
             }
-            'Detail*' {
-                # Return account details for each AccountID specified
-                $CategoryID.ForEach{
-                    $response = Invoke-RestMethod "$uri/budgets/$BudgetID/categories/$_" -Headers $header
-                    if ($response) {
-                        Get-ParsedCategoryJson $response.data.category
-                    }
+            'Detail' {
+                $categories = [Array]((Get-YnabCategory -Budget $Budget -ListAll -IncludeHidden:$IncludeHidden -Token $Token).Categories)
+                # Return category details for each category specified
+                foreach ($categoryName in $Category) {
+                    $categoryId = $categories.Where{$_.Category -like $categoryName}.CategoryID
+                    $response = Invoke-RestMethod "$uri/budgets/$budgetId/categories/$categoryId" -Headers $header
+                    Get-ParsedCategoryJson $response.data.category
                 }
             }
         }
