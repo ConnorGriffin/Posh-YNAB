a9aff80e7b1c89a9a682707f3b1b01b6d3a9ccd0
Connor Griffin <connor@connorcg.com>
2018-06-03 22:55:47 -0700

    Cleaned up parameter sets, added pipeline support

--- Posh-YNAB/Public/Add-YnabTransactionPreset.ps1
+++ Posh-YNAB/Public/Add-YnabTransactionPreset.ps1
@@ -8,122 +8,95 @@ function Add-YnabTransactionPreset {
     .EXAMPLE
     Add-YnabTransactionPreset -PresetName 'Coffee' -BudgetName 'TestBudget' -AccountName 'Checking' -CategoryName 'Food' -Memo 'Coffee' -Outflow 3.50
     Adds a transaction preset called Coffee that can be used in Add-YnabTransaction with Add-YnabTransaction -PresetName 'Coffee'
-    .PARAMETER PresetName
-    The name of the preset to remove, accepts a string or array of strings. Supports wildcards.
-    .PARAMETER BudgetName
-    The name of the budget to add the transaction to.
-    .PARAMETER BudgetID
-    The ID of the budget to add the transaction to.
-    Takes priority over BudgetName if both are provided.
-    .PARAMETER AccountName
-    The name of the account to add the transaction to.
-    .PARAMETER AccountID
-    The ID of the account to add the transaction to.
-    Takes priority over AccountName if both are provided.
-    .PARAMETER PayeeName
-    The name of the payee to add the transaction to.
-    .PARAMETER PayeeID
-    The ID of the payee to add the transaction to.
-    Takes priority over PayeeName if both are provided.
-    .PARAMETER CategoryName
-    The name of the category to add the transaction to.
-    .PARAMETER CategoryID
-    The ID of the category to add the transaction to.
-    Takes priority over CategoryName if both are provided.
-    .PARAMETER Memo
-    Memo for the transaction.
-    .PARAMETER Outflow
-    Outflow amount for the transaction.
-    Uses absolute value, so a positive or negative number can be provided.
-    .PARAMETER Inflow
-    Inflow amount for the transaction.
-    Uses absolute value, so a positive or negative number can be provided.
-    .PARAMETER Amount
-    Amount for the transaction.
-    Negative = Outflow, Positive = Inflow
-    .PARAMETER Date
-    Date for the trarnsaction.
-    Defaults to today.
-    .PARAMETER Token
-    API token used to post the transaction.
-    .PARAMETER FlagColor
-    Flag color for the transaction.
-    .PARAMETER Cleared
-    If specified the transaction will be marked as CLeared.
-    .PARAMETER Approved
-    If specified the transaction will be marked as Approved.
-    Defaults to $true.
     #>
-    [CmdletBinding(DefaultParameterSetName='NoAmount')]
+    [CmdletBinding(DefaultParameterSetName='Preset')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification='API key are provided as plaintext (See -NuGetApiKey for Publish-Module), so this is actually improving security by storing the keys as a SecureString. AWS has CLI tools that store API keys in plaintext files in a ~\.aws\ folder, for example. See also: https://github.com/PowerShell/PSScriptAnalyzer/issues/574')]
     param(
-        [Parameter(Mandatory=$true,Position=0)]
-        [Alias('Preset')]
-        [String]$PresetName,
+        # The name to save the preset with (see: Add-YnabTransactionPreset).
+        [Parameter(Mandatory,
+                   Position=0,
+                   ValueFromPipelineByPropertyName)]
+        [Alias('Name','PresetName')]
+        [String]$Preset,
 
-        [Parameter(Position=10)]
-        [Alias('Budget')]
-        [String]$BudgetName,
+        # The name of the budget to add the transaction to.
+        [Parameter(Position=1,
+                   ValueFromPipelineByPropertyName)]
+        [String]$Budget,
 
-        [Parameter(Position=11,DontShow)]
-        [String]$BudgetID,
+        # The name of the account to add the transaction to.
+        [Parameter(Position=2,
+                   ValueFromPipelineByPropertyName)]
+        [String]$Account,
 
-        [Parameter(Position=20)]
-        [Alias('Account')]
-        [String]$AccountName,
+        # The name of the payee to add the transaction to.
+        [Parameter(Position=3,
+                   ValueFromPipelineByPropertyName)]
+        [String]$Payee,
 
-        [Parameter(Position=21,DontShow)]
-        [String]$AccountID,
+        # The name of the category to add the transaction to.
+        [Parameter(Position=4,
+                   ValueFromPipelineByPropertyName)]
+        [String]$Category,
 
-        [Parameter(Position=30)]
-        [Alias('Payee')]
-        [String]$PayeeName,
-
-        [Parameter(Position=31,DontShow)]
-        [String]$PayeeID,
-
-        [Parameter(Position=40)]
-        [Alias('Category')]
-        [String]$CategoryName,
-
-        [Parameter(Position=41,DontShow)]
-        [String]$CategoryID,
-
-        [Parameter(Position=50)]
+        # Memo for the transaction.
+        [Parameter(Position=5,
+                   ValueFromPipelineByPropertyName)]
         [String]$Memo,
 
-        [Parameter(Mandatory=$true,Position=60,ParameterSetName='Outflow')]
-        [Parameter(Mandatory=$false,Position=60,ParameterSetName='NoAmount')]
+        # Outflow amount for the transaction.
+        # Uses absolute value, so a positive or negative number can be provided.
+        [Parameter(Mandatory,
+                   Position=6,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName='Preset,Outflow')]
         [Double]$Outflow,
 
-        [Parameter(Mandatory=$true,Position=61,ParameterSetName='Inflow')]
-        [Parameter(Mandatory=$false,Position=61,ParameterSetName='NoAmount')]
+        # Inflow amount for the transaction.
+        # Uses absolute value, so a positive or negative number can be provided.
+        [Parameter(Mandatory,
+                   Position=6,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName='Preset,Inflow')]
         [Double]$Inflow,
 
-        [Parameter(Mandatory=$true,Position=62,ParameterSetName='Amount')]
-        [Parameter(Mandatory=$false,Position=62,ParameterSetName='NoAmount')]
+        # Amount for the transaction. Negative = Outflow, Positive = Inflow
+        [Parameter(Mandatory,
+                   Position=6,
+                   ValueFromPipelineByPropertyName,
+                   ParameterSetName='Preset,Amount')]
         [Double]$Amount,
 
-        [Parameter(Position=70)]
-        [Datetime]$Date = (Get-Date),
+        # Date for the trarnsaction. 
+        # Defaults to today.
+        [Parameter(Position=7,
+                   ValueFromPipelineByPropertyName)]
+        [Datetime]$Date,
 
-        [Parameter(Position=80)]
+        # YNAB API token.
+        [Parameter(Position=8,
+                   ValueFromPipelineByPropertyName)]
         $Token,
 
-        [Parameter(Position=90)]
+        # Flag color for the transaction.
+        [Parameter(Position=9,
+                   ValueFromPipelineByPropertyName)]
         [ValidateSet('Red','Orange','Yellow','Green','Blue','Purple')]
         [String]$FlagColor,
 
-        [Parameter(Position=100)]
+        # If specified the transaction will be marked as CLeared.
+        [Parameter(Position=10,
+                   ValueFromPipelineByPropertyName)]
         [Switch]$Cleared,
 
-        [Parameter(Position=110)]
+        # If specified the transaction will be marked as Approved.
+        # Defaults to $true.
+        [Parameter(Position=11,
+                   ValueFromPipelineByPropertyName)]
         [Bool]$Approved=$true
     )
 
     begin {
-        Write-Verbose "New-YnabTransactionPreset.ParameterSetName: $($PsCmdlet.ParameterSetName)"
-
         # Encrypt the token if it is of type String, replace $PSBoundParameters.Token with the SecureString version
         $data = $PSBoundParameters
         if ($Token.GetType().Name -eq 'String') {
@@ -140,15 +113,43 @@ function Add-YnabTransactionPreset {
     }
 
     process {
-        # Get the preset name and then remove it from the parameters array
-        $name = $data.PresetName
-        [Void]$data.Remove('PresetName')
-
         # Remove the preset from the hashtable (does nothing if it does not exist)
-        $presets.Remove($name)
+        $presets.Remove($Preset)
+
+        # Add all of the parameter values to a hashtable. Can't use $PSBoundParameters because it breaks pipeline support :( 
+        $data = @{
+            Budget = $Budget 
+            Account = $Account 
+            Payee = $Payee
+            Category = $Category 
+            Memo = $Memo 
+            Outflow = $Outflow 
+            Inflow = $Inflow 
+            Amount = ($Amount * 1000)
+            Date = $Date 
+            Token = $Token 
+            FlagColor = $FlagColor 
+            Cleared = $Cleared 
+            Approved = $Approved 
+        }
+
+        # Determine null entries to remove (easier than only adding non-null to $data with a ton of ifs)
+        $remove = $data.GetEnumerator().ForEach{
+            if (!$_.Value) {
+                $_.Key
+            }
+        }
+
+        # Actually remove the values (can't modify $data during a foreach)
+        $remove.ForEach{
+            $data.Remove($_)
+        }
 
         # Add the preset data to the presets hashtable, then export to $presetFile
-        $presets += @{$name = $data}
+        $presets += @{$Preset = $data}
+    } 
+
+    end {
         $presets | Export-Clixml $presetFile
     }
 }
