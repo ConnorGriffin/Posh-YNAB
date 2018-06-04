dd9a1cf61fbcfae28d572158a6c7ee3ef4f9ccd8
Connor Griffin <connor@connorcg.com>
2018-06-04 01:19:44 -0700

    Added manifest test output

--- CodeDeploy.ps1
+++ CodeDeploy.ps1
@@ -5,6 +5,7 @@ Switch ($Phase) {
     'Install' {
     }
     'Build' {
+        Test-ModuleManifest -Path ./Posh-YNAB/Posh-YNAB.psd1 -Verbose
         Publish-Module -Path ./Posh-YNAB/ -NugetApiKey $ENV:PSGalleryAPIKey
     }
 }
