a62dd38241665840f41aa3d522ec6c448b6204c9
Connor Griffin <connor@connorcg.com>
2018-06-09 02:24:49 -0700

    Added dynamic public function export tests

--- Tests/Posh-YNAB.Tests.ps1
+++ Tests/Posh-YNAB.Tests.ps1
@@ -35,6 +35,38 @@ $transactionHashtable = @{
 $defaultParam = $PSDefaultParameterValues["Disabled"]
 $PSDefaultParameterValues["Disabled"] = $true
 
+
+Describe 'Public functions exports' {
+    $files = Get-ChildItem -Path .\Posh-YNAB\Public\*.ps1
+    $exportedFunctions = (Get-Module -Name Posh-YNAB).ExportedFunctions.Values.Name
+    
+    Context 'File names match the function they export' {
+        $files.ForEach{
+            It "$($_.Name) exports $($_.BaseName)" {
+                $content = Get-Content $_.FullName 
+                $function = $content[0].Split(' ')[1]
+                $function | Should -Be $_.BaseName
+            }
+        }
+    }
+
+    Context 'All public files correspond to an exported function' {
+        $files.ForEach{
+            It "$($_.BaseName) is in exported functions" {
+                $_.BaseName | Should -BeIn $exportedFunctions
+            }
+        }
+    }
+
+    Context 'All exported functions correspond to a public file' {
+        $exportedFunctions.ForEach{
+            It "$_.ps1 is in Public" {
+                ".\Posh-YNAB\Public\$_.ps1" | Should -Exist
+            }
+        }
+    }
+}
+
 Describe 'Add-YnabTransaction' {
     # Force 
     InModuleScope Posh-YNAB {
